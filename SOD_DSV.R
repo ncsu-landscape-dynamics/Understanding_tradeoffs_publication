43482e6984e0760791ef3ef01777b1d66e34e4bd
John Polo <68556651+John-Polo@users.noreply.github.com>
2023-11-17 17:09:22 -0500

    Update SOD_DSV.R
    Combined some loops.

--- SOD_DSV.R
+++ SOD_DSV.R
@@ -1,6 +1,7 @@
 library(terra)
 library(tigris)
 
+# Calculations to set up the DSV scores.
 # Formula from Meentemeyer et al. 2011
 sodweather <- function(x){
   y = -0.066 + 0.056*x - 0.0036*(x - 15)^2 - 0.0003*(x - 15)^3
@@ -13,21 +14,28 @@ sodvf2 <- data.frame(t = seq(-2,32,by=0.25), dv = unlist(sodv2))
 
 # The coefficient has a hump at small, positive temps.
 # Remove the hump and use a line.
-xs <- c(0,0.25,0.5,0.75,1,1.25,1.5,1.75,2,2.25,2.5)
-yx <- c(0.1365, 0.12749, 0.11969, 0.11307, 0.1076, 0.10326,0.1,0.0978, 0.0967, 0.0966,0.0974)
+xs <- c(0,0.25,0.5,0.75,1,1.25,1.5,1.75,2,2.25,2.5,2.75)
+yx <- c(0.1365, 0.12749, 0.11969, 0.11307, 0.1076, 0.10326,0.1,0.0978, 0.0967, 0.0966,0.0974,0.099255)
 m1 <- lm(yx ~ xs)
 summary(m1)
 
-# Simplify the SOD data frame
+xs <- c(0,3.5)
+yx <- c(0, 0.115)
+m1 <- lm(yx ~ xs)
+summary(m1)
+
+# Simplify the SOD data frame, since all of the T < 0 have no impact on DSV.
 sodvf2[sodvf2$t < 0, "dv"] <- 0 
-sodvf2[4,] <- c(1, 1*0.04835)
 sodvf2 <- sodvf2[sodvf2$dv > 0.0,]
-sodvf2[9:19, "dv"] <- sodvf2[9:19, "t"] * 0.0155
+sodvf2[1:15, "dv"] <- sodvf2[1:15, "t"] * 0.0338
 
-thresh1 <- 0.25*max(sodvf2$dv)
-thresh2 <- 0.5*max(sodvf2$dv)
-thresh3 <- 0.75*max(sodvf2$dv)
+# Calculate the thresholds
+thresh1 <- 0.2*max(sodvf2$dv) # 0.1853247
+thresh2 <- 0.4*max(sodvf2$dv) # 0.3706494
+thresh3 <- 0.6*max(sodvf2$dv) # 0.5559741
+thresh4 <- 0.8*max(sodvf2$dv) # 0.7412988
 
+# DSV logic
 dsv <- function(precip, tmean) {
   prec_val = ifel(precip >= 2, 1, 0)
   
@@ -41,7 +49,7 @@ dsv <- function(precip, tmean) {
   return(dsv_val)
 }
 
-# Geographical focus
+# Geographical focus for SOD
 co1 <- counties(state = c("CA","OR"))
 co1 <- co1[co1$NAME %in% c("Coos","Curry","Douglas","Josephine"),]#,"Del Norte"),]
 
@@ -67,7 +75,6 @@ dsvf <- function(i){
   precip = crop(pr, co1, mask=T)
   
   tmean = (mx+mn)/2
-  print(tmean)
   
   codsv = dsv(precip, tmean)
   return(codsv)
@@ -95,36 +102,29 @@ for(i in 1:12){
   res1[i] = resample(temr, r3, method="near")
 }
 
-# Get the list of files for revising further.
-l1 <- list.files("Q:/My Drive/temp/",pattern="soddsvdaily.*tif$", full.names = T)
-
 # Fix some values that resampled as decimal
 rm1 <- matrix(c(0,0.5,0,0.5,1,1,1,1.5,1,1.5,2,2,2,2.5,2,2.5,3,3,3,3.5,3,3.5,4,4),
               ncol=3, byrow = T)
 
-for(i in 1:length(l1)){
-  temr = rast(l1[i])
-  temr2 = classify(temr, rm1, right=T)
-  writeRaster(temr2, paste0("Q:/My Drive/temp/soddsvdailyrev_", yseq[i],".tif"))
+temr2 = list()
+
+for(i in 1:length(res1)){
+  temr2[i] = classify(temr, rm1, right=T)
 }
 
 # Cumulative sum of the daily values in DSV.
-cd1 <- list()
-
-l1 <-l1[seq(1,24, by=2)]
-
-for(i in 1:12){
-  cd1[i] = app(rast(l1[i]), cumsum)
-}
-
 # Cumulative counts of DSV/18
-cd2 <- list()
+yseq <- 2009:2020
 
 for(i in 1:12){
-  cd2[i] = app(rast(cd1[i]), function(x) floor(x / 18))
+  l1 <- list.files(paste0("Q:/My Drive/popblightdata/newareas/DSV/",yseq[i],"/daily/"), full.names = T)
+  l2 <- l1[c(1, 112, 223, 300, 311, 322, 333, 344, 355, 2, 13, 24, 35, 46, 57, 68, 79, 90, 101, 113, 124, 135, 146, 157, 168, 179, 190, 201, 212, 224, 235, 246, 257, 268, 279, 290, 297:299, 301:310, 312:321, 323:332, 334:343, 345:354, 356:365, 3:12, 14:23, 25:34, 36:45, 47:56, 58:67, 69:78, 80:89, 91:100, 102:111, 114:123, 125:134, 136:145, 147:156, 158:167, 169:178, 180:189, 191:200, 202:211, 213:222, 225:234, 236:245, 247:256, 258:267, 269:278, 280:289, 291:296)]
+  cd1 <- app(rast(l1), cumsum)
+  cd2 <- app(cd1, function(x) floor(x/18))
+  for(j in 1:365){
+    jpeg(paste0("Q:/My Drive/DSV/",yseq[i],"/cumulthreshold/cuthresh",yseq[i],"_",j,".jpg"))
+    plot(cd2[[j]])
+    dev.off()
+  }
+  writeRaster(cd2, paste0("Q:/My Drive/DSV/",yseq[i],"/cuthresh",yseq[i],"_",j,".tif"))
 }
-
-for(i in 1:12){
-writeRaster(rast(cd2[i]), paste0("Q:/My Drive/temp/cumudsv_18_",yseq[i],".tif"))
-}
-
