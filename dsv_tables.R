764bf42591fa0694d00c573f1f6ac15fa1ac1c42
John Polo <68556651+John-Polo@users.noreply.github.com>
2024-05-22 07:26:46 -0400

    Create dsv_tables.R
    Process rasters of DSV to get the values to make box and whisker plots. The process is memory intensive, so the box and whisker plots have to be created separated.

---
+++ dsv_tables.R
@@ -0,0 +1,48 @@
+library(data.table)
+library(terra)
+library(tidyverse)
+
+# LB DSV raster file list
+lh1 <- list.files("Z:/Late_blight/lbdsveconu/", pattern = "zone", full.names = T, recursive = T)
+lh1 <- lh1[20:247]
+lh1 <- gtools::mixedsort(lh1)
+
+# Divide lh1 into lists based on hardiness zone and year
+# Years
+names_per_sublist <- 12
+
+# Create a sequence of group numbers for each file name
+groups <- ceiling(seq_along(lh1) / names_per_sublist)
+
+# Split the file names into sublists based on the group numbers
+sublists <- split(lh1, groups)
+
+# Move through the layers and get the DSVs
+yearlyval <- function(subind, dayind) {
+  # Raster
+  raszone = rast(sublists[[subind]])
+  
+  # Sequence for day of year
+  dayseq = seq(dayind, nlyr(raszone), by = 365)
+  
+  # Values
+  dayvalue = values(raszone[[dayseq]], na.rm=T, dataframe =T)
+  colnames(dayvalue) = paste0("y",2009:2020)
+  dayvalue$day = dayind
+  
+  return(dayvalue)
+}
+
+# Apply the function through the classes of lists
+yearcol_list <- lapply(1:19, function(subind) {
+  yearcol_sub <- lapply(1:365, function(dayind) yearlyval(subind, dayind))
+  yearwrite = do.call(rbind, yearcol_sub)
+  write.csv(yearwrite, paste0("Z:/Late_blight/lbdsveconu/appscores/csv/sub_",subind,".csv"),
+            row.names = F)
+})
+
+# The CSVs written are huge... up to 6GB. Manipulating them on local desktop is difficult,
+# even with data.table. Out of memory errors are returned. 
+
+
+
