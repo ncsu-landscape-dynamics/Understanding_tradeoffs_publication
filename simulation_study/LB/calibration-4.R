1dbe9684f6c397bfdf026736170e0173f242ab48
John Polo <68556651+John-Polo@users.noreply.github.com>
2025-09-17 09:56:39 -0400

    Refactor calibration configuration and parameters

--- simulation_study/LB/calibration-4.R
+++ simulation_study/LB/calibration-4.R
@@ -1,95 +1,187 @@
 library(PoPS)
-root <- "Z:/Late_Blight/Manuscript_1_Data/simulation/LB"
-inpath <- file.path(root, "inputs")
+library(terra)
+library(doParallel)
+library(foreach)
 
-config <- list()
+rootdir <- "Z:/Late_blight/Manuscript_1_Data/simulation/LB"
 
-# Assign values to config
-config$random_seed <- NULL
-config$infected_file_list <- file.path(inpath, "infection", "infection_2021.tif")
-config$host_file_list <- file.path(inpath, "host", "host_no_na_rv_2021.tif")
-config$total_populations_file <- file.path(inpath, "all_populations", "tot_pop_no_na_rv_2021.tif")
-config$parameter_means <- t(read.table(file.path(inpath, "parameters", "lb_means_upwardrev.csv")))[1, ]
-config$parameter_cov_matrix <- read.table(file.path(inpath, "parameters", "lb_cov_mat.csv"))
-config$temp <- TRUE
-config$temperature_coefficient_file <- file.path(inpath, "weather", "wx_coef", "lb_2021_coef_avg.tif")
-config$precip <- FALSE
-config$precipitation_coefficient_file <- ""
-config$model_type <- "SI"
-config$latency_period <- 0
-config$time_step <- "week"
-config$season_month_start <- 1
-config$season_month_end <- 12
-config$start_date <- "2021-01-01"
-config$end_date <- "2021-12-31"
-config$use_lethal_temperature <- FALSE
-config$temperature_file <- ''
-config$lethal_temperature <- 3
-config$lethal_temperature_month <- 1
-config$use_survival_rates <- FALSE
-config$survival_rate_month <- 3
-config$survival_rate_day <- 15
-config$survival_rates_file <- ""
-config$mortality_on <- FALSE
-config$mortality_rate <- 0
-config$mortality_time_lag <- 0
-config$mortality_frequency <- "year"
-config$mortality_frequency_n <- 1
-config$management <- FALSE
-config$treatment_dates <- ""#c("2021-12-24")
-config$treatments_file <- ""
-config$treatment_method <- "ratio"
-config$natural_kernel_type <- "cauchy"
-config$anthropogenic_kernel_type <- "cauchy"
-config$natural_dir <- "NONE"
-config$anthropogenic_dir <- "NONE"
-config$pesticide_duration <- c(0)
-config$pesticide_efficacy <- 1.0
-config$output_frequency <- "year"
-config$output_frequency_n <- 1
-config$movements_file <- ""
-config$use_movements <- FALSE
-config$start_exposed <- FALSE
-config$generate_stochasticity <- TRUE
-config$establishment_stochasticity <- TRUE
-config$movement_stochasticity <- TRUE
-config$dispersal_stochasticity <- TRUE
-config$establishment_probability <- 0.5
-config$dispersal_percentage <- 0.99
-config$quarantine_areas_file <- ""
-config$quarantine_directions <- ""
-config$use_quarantine <- FALSE
-config$use_spreadrates <- FALSE
-config$use_overpopulation_movements <- FALSE
-config$overpopulation_percentage <- 0.75
-config$leaving_percentage <- 0.5
-config$leaving_scale_coefficient <- 5
-config$number_of_iterations <- 4000
-config$number_of_cores <- 5
-config$function_name <- "multirun"
-config$failure <- NULL
-config$exposed_file_list <- ""
-config$mask <- NULL
-config$write_outputs <- "summary_outputs"
-config$output_folder_path <- file.path(root, "outputs21/locs6x2/upwdrev")
-config$mortality_frequency <- "year"
-config$mortality_frequency_n <- 1
-config$network_filename <- ''
-config$network_movement <- "walk"
-config$use_initial_condition_uncertainty <- FALSE
-config$use_host_uncertainty <- FALSE
-config$weather_type <- "deterministic"
-config$temperature_coefficient_sd_file <- ""
-config$precipitation_coefficient_sd_file <- ""
-config$dispersers_to_soils_percentage <- 0
-config$multiple_random_seeds <- TRUE
-config$file_random_seeds <- NULL
-config$use_soils <- FALSE
-config$soil_starting_pest_file <- ""
-config$start_with_soil_populations <- FALSE
-config$county_level_infection_data <- FALSE
-config$pest_host_table <- file.path(inpath, "host", "pest_host_table_LB.csv")
-config$competency_table <- file.path(inpath, "host", "competency_table_LB.csv")
+#infected_years_file <- file.path(rootdir, "inputs/infection/infection_2023.tif")
+infected_years_file <- file.path(rootdir, "outputs21/locs6x2/upwdrev/rasts/pops_median_Year_2021.tif")
+number_of_observations = 234
+prior_number_of_observations = 0
+prior_means = file.path(rootdir, "inputs/parameters/lb_means_upwardrev.csv")
+prior_cov_matrix = file.path(rootdir, "inputs/parameters/lb_cov_mat.csv")
+params_to_estimate = c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE)
+number_of_generations = 7
+generation_size = 1000
+pest_host_table = file.path(rootdir, "inputs/host/pest_host_table_LB.csv")
+competency_table = file.path(rootdir, "inputs/host/competency_table_LB.csv")
+infected_file_list <- file.path(rootdir, "outputs21/locs6x2/upwdrev/rasts/pops_median_Year_2021.tif")
+host_file_list <- file.path(rootdir, "inputs/host/host_no_na_rv_2021.tif")
+total_populations_file <- file.path(rootdir, "inputs/all_populations/tot_pop_no_na_rv_2021.tif")
+temp = TRUE
+temperature_coefficient_file = file.path(rootdir, "inputs/weather/wx_coef/lb_2021_coef_avg.tif")
+precip = FALSE
+precipitation_coefficient_file = ""
+model_type = "SI"
+latency_period = 0
+time_step = "week"
+season_month_start = 1
+season_month_end = 12
+start_date = "2021-01-01"
+end_date = "2021-12-31"
+use_survival_rates = FALSE
+survival_rate_month = 3
+survival_rate_day = 15
+survival_rates_file = ""
+use_lethal_temperature = FALSE
+temperature_file = ""
+lethal_temperature = 3
+lethal_temperature_month = 1
+mortality_frequency = "year"
+mortality_frequency_n = 1
+management = FALSE
+treatment_dates = c("")
+treatments_file = ""
+treatment_method = "ratio"
+natural_kernel_type = "cauchy"
+anthropogenic_kernel_type = "cauchy"
+natural_dir = "NONE"
+natural_kappa = 0
+anthropogenic_dir = "NONE"
+anthropogenic_kappa = 0
+pesticide_duration = c(0)
+pesticide_efficacy = 1
+mask = NULL
+output_frequency = "year"
+output_frequency_n = 1
+movements_file = ""
+use_movements = FALSE
+start_exposed = FALSE
+generate_stochasticity = TRUE
+establishment_stochasticity = TRUE
+movement_stochasticity = TRUE
+dispersal_stochasticity = TRUE
+establishment_probability = 0.5
+dispersal_percentage = 0.99
+quarantine_areas_file = ""
+use_quarantine = FALSE
+use_spreadrates = FALSE
+use_overpopulation_movements = FALSE
+overpopulation_percentage = 0
+leaving_percentage = 0
+leaving_scale_coefficient = 1
+calibration_method = "ABC"
+number_of_iterations = 1e+05
+exposed_file_list = ""
+verbose = TRUE
+write_outputs = "summary_outputs"#"None"
+output_folder_path = file.path(rootdir, "outputs21/locs6x2/upwdrev/calib")
+network_filename = ""
+network_movement = "walk"
+success_metric = "mcc"
+use_initial_condition_uncertainty = FALSE
+use_host_uncertainty = FALSE
+weather_type = "deterministic"
+temperature_coefficient_sd_file = ""
+precipitation_coefficient_sd_file = ""
+dispersers_to_soils_percentage = 0
+quarantine_directions = ""
+multiple_random_seeds = FALSE
+file_random_seeds = NULL
+use_soils = FALSE
+soil_starting_pest_file = ""
+start_with_soil_populations = FALSE
+county_level_infection_data = FALSE
 
-config <- configuration(config)
-saveRDS(config, file.path(inpath, "config_LB_21_sim_locs6_up.rds"))
+
+calibrate(
+  infected_years_file = infected_years_file,
+  number_of_observations = number_of_observations,
+  prior_number_of_observations = 0,
+  prior_means = prior_means,
+  prior_cov_matrix = prior_cov_matrix,
+  params_to_estimate = params_to_estimate,
+  number_of_generations = number_of_generations,
+  generation_size = generation_size,
+  pest_host_table = pest_host_table,
+  competency_table = competency_table,
+  infected_file_list = infected_file_list,
+  host_file_list = host_file_list,
+  total_populations_file = total_populations_file,
+  temp = TRUE,
+  temperature_coefficient_file = "",
+  precip = FALSE,
+  precipitation_coefficient_file = "",
+  model_type = "SI",
+  latency_period = 0,
+  time_step = time_step,
+  season_month_start = 1,
+  season_month_end = 12,
+  start_date = "2021-01-01",
+  end_date = "2021-12-31",
+  use_survival_rates = FALSE,
+  survival_rate_month = 3,
+  survival_rate_day = 15,
+  survival_rates_file = "",
+  use_lethal_temperature = FALSE,
+  temperature_file = "",
+  lethal_temperature = -12.87,
+  lethal_temperature_month = 1,
+  mortality_frequency = "year",
+  mortality_frequency_n = 1,
+  management = FALSE,
+  treatment_dates = c(""),
+  treatments_file = "",
+  treatment_method = "ratio",
+  natural_kernel_type = "cauchy",
+  anthropogenic_kernel_type = "cauchy",
+  natural_dir = "NONE",
+  natural_kappa = 0,
+  anthropogenic_dir = "NONE",
+  anthropogenic_kappa = 0,
+  pesticide_duration = c(0),
+  pesticide_efficacy = 1,
+  mask = NULL,
+  output_frequency = "year",
+  output_frequency_n = 1,
+  movements_file = "",
+  use_movements = FALSE,
+  start_exposed = FALSE,
+  generate_stochasticity = TRUE,
+  establishment_stochasticity = TRUE,
+  movement_stochasticity = TRUE,
+  dispersal_stochasticity = TRUE,
+  establishment_probability = 0.5,
+  dispersal_percentage = 0.99,
+  quarantine_areas_file = "",
+  use_quarantine = FALSE,
+  use_spreadrates = FALSE,
+  use_overpopulation_movements = FALSE,
+  overpopulation_percentage = 0,
+  leaving_percentage = 0,
+  leaving_scale_coefficient = 1,
+  calibration_method = "ABC",
+  number_of_iterations = 1e+05,
+  exposed_file_list = "",
+  verbose = TRUE,
+  write_outputs = write_outputs,
+  output_folder_path = output_folder_path,
+  network_filename = "",
+  network_movement = "walk",
+  success_metric = "mcc",
+  use_initial_condition_uncertainty = FALSE,
+  use_host_uncertainty = FALSE,
+  weather_type = "deterministic",
+  temperature_coefficient_sd_file = "",
+  precipitation_coefficient_sd_file = "",
+  dispersers_to_soils_percentage = 0,
+  quarantine_directions = "",
+  multiple_random_seeds = FALSE,
+  file_random_seeds = NULL,
+  use_soils = FALSE,
+  soil_starting_pest_file = "",
+  start_with_soil_populations = FALSE,
+  county_level_infection_data = FALSE
+)
+# 
