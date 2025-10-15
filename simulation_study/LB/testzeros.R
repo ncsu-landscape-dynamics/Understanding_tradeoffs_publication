11de817d41c9f6ddff365b1fa8386dae0d38eafe
John Polo <68556651+John-Polo@users.noreply.github.com>
2025-10-15 10:01:39 -0400

    Update testzeros.R

--- simulation_study/LB/testzeros.R
+++ simulation_study/LB/testzeros.R
@@ -1,3 +1,96 @@
+rootdir <- "Z:/Late_blight/Manuscript_1_Data/simulation/LB"
+
+#infected_years_file <- file.path(rootdir, "inputs/infection/infection_2023.tif")
+infected_years_file <- file.path(rootdir, "inputs/infection/testzero.tif")
+number_of_observations = 356
+prior_number_of_observations = 0
+prior_means = file.path(rootdir, "inputs/parameters/lb_means_upwardrev.csv")
+prior_cov_matrix = file.path(rootdir, "inputs/parameters/lb_cov_mat.csv")
+params_to_estimate = c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE)
+number_of_generations = 7
+generation_size = 1000
+pest_host_table = file.path(rootdir, "inputs/host/pest_host_table_LB.csv")
+competency_table = file.path(rootdir, "inputs/host/competency_table_LB.csv")
+infected_file_list <- file.path(rootdir, "inputs/infection/infection_2021_rev_10locs.tif")
+host_file_list <- file.path(rootdir, "inputs/host/host_no_na_rv_2021.tif")
+total_populations_file <- file.path(rootdir, "inputs/all_populations/tot_pop_no_na_rv_2021.tif")
+temp = TRUE
+temperature_coefficient_file = file.path(rootdir, "inputs/weather/wx_coef/lb_2021_coef_avg.tif")
+precip = FALSE
+precipitation_coefficient_file = ""
+model_type = "SI"
+latency_period = 0
+time_step = "week"
+season_month_start = 1
+season_month_end = 12
+start_date = "2021-01-01"
+end_date = "2021-12-31"
+use_survival_rates = FALSE
+survival_rate_month = 3
+survival_rate_day = 15
+survival_rates_file = ""
+use_lethal_temperature = FALSE
+temperature_file = ""
+lethal_temperature = 3
+lethal_temperature_month = 1
+mortality_frequency = "year"
+mortality_frequency_n = 1
+management = FALSE
+treatment_dates = c("")
+treatments_file = ""
+treatment_method = "ratio"
+natural_kernel_type = "cauchy"
+anthropogenic_kernel_type = "cauchy"
+natural_dir = "NONE"
+natural_kappa = 0
+anthropogenic_dir = "NONE"
+anthropogenic_kappa = 0
+pesticide_duration = c(0)
+pesticide_efficacy = 1
+mask = NULL
+output_frequency = "year"
+output_frequency_n = 1
+movements_file = ""
+use_movements = FALSE
+start_exposed = FALSE
+generate_stochasticity = TRUE
+establishment_stochasticity = TRUE
+movement_stochasticity = TRUE
+dispersal_stochasticity = TRUE
+establishment_probability = 0.5
+dispersal_percentage = 0.99
+quarantine_areas_file = ""
+use_quarantine = FALSE
+use_spreadrates = FALSE
+use_overpopulation_movements = FALSE
+overpopulation_percentage = 0
+leaving_percentage = 0
+leaving_scale_coefficient = 1
+calibration_method = "ABC"
+number_of_iterations = 1e+05
+exposed_file_list = ""
+verbose = TRUE
+write_outputs = "all_simulations"#"None"
+output_folder_path = file.path(rootdir, "outputs21/locs10x2/upwdrev/calib")
+network_filename = ""
+network_movement = "walk"
+#success_metric = "quantity and rmse"  OLD PoPS
+success_metric = "mcc"
+use_initial_condition_uncertainty = FALSE
+use_host_uncertainty = FALSE
+weather_type = "deterministic"
+temperature_coefficient_sd_file = ""
+precipitation_coefficient_sd_file = ""
+dispersers_to_soils_percentage = 0
+quarantine_directions = ""
+multiple_random_seeds = FALSE
+file_random_seeds = NULL
+use_soils = FALSE
+soil_starting_pest_file = ""
+start_with_soil_populations = FALSE
+county_level_infection_data = FALSE
+
+
 cal2110_zerorq <- calibrate(
   infected_years_file = infected_years_file,
   number_of_observations = number_of_observations,
