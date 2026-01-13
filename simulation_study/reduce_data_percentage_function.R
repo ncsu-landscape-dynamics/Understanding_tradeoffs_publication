3e326ae4d0aea989fe2f6d20aacac1f534bd4565
John Polo <68556651+John-Polo@users.noreply.github.com>
2026-01-13 11:17:05 -0500

    Update reduce_data_percentage_function.R

--- simulation_study/reduce_data_percentage_function.R
+++ simulation_study/reduce_data_percentage_function.R
@@ -1,17 +1,17 @@
 library(terra)
 
  
-location_reduction <- function(cells, x, inst) {
+location_reduction <- function(cells, reduce_percent, inst) {
   r_tem = red_raster
-  # Pick X% of cells
-  smlset = sample(cells, round(length(cells)*x))
+  # Pick reduce %age of cells
+  smlset = sample(cells, round(length(cells)*reduce_percent))
   
-  print(paste0("x: ", x))
+  print(paste0("reduce percent: ", reduce_percent))
   print(length(smlset))
   # Set selected cells to same value as background
   r_tem[smlset] = 0
   
-  pct = 100 - (x*100)
+  pct = 100 - (reduce_percent*100)
   print(paste0(outputpath, pct, "/TEST_", pct, "-", inst, "round.tif"))
 
   writeRaster(r_tem, paste0(outputpath, pct, "/TEST_", pct, "-", inst, 
